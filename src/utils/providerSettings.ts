816b5861725b4c39be632276b7256b89ae271a3d
kobbin003 <dkfeto@gmail.com>
2025-06-16 17:03:38 +0530

    scope for facebook added

--- src/utils/providerSettings.ts
+++ src/utils/providerSettings.ts
@@ -14,5 +14,6 @@ export const providerOauth2Settings: Partial<
 		tokenPath: "/v15.0/oauth/access_token", // or latest version (e.g. /v19.0/)
 		authorizePath: "/v15.0/dialog/oauth",
 		redirectUri: `${process.env.BASE_URL}/api/auth/callback/google`,
+		scope: "email public_profile",
 	},
 };
