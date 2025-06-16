f4b4395a0c1fb87b78a5bc802517e99dc9977547
kobbin003 <dkfeto@gmail.com>
2025-06-16 17:07:21 +0530

    facebook authorizePath added

--- src/utils/providerSettings.ts
+++ src/utils/providerSettings.ts
@@ -12,6 +12,7 @@ export const providerOauth2Settings: Partial<
 	facebook: {
 		tokenHost: "https://graph.facebook.com",
 		tokenPath: "/v15.0/oauth/access_token", // or latest version (e.g. /v19.0/)
+		authorizeHost: "https://www.facebook.com",
 		authorizePath: "/v15.0/dialog/oauth",
 		redirectUri: `${process.env.BASE_URL}/api/auth/callback/google`,
 		scope: "email public_profile",
